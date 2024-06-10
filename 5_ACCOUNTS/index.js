b6c341645154d130ddcb4741d3e874440aa8f439
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-06-10 10:46:12 -0300

    Lesson 77

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -5,25 +5,70 @@ const chalk = require("chalk");
 // internal modules
 const fs = require("fs");
 
-inquirer.prompt([
-  {
-    type: "list",
-    name: "action",
-    message: "What do you want to do?",
-    choices: ["Create an Account", "Balance", "Deposit", "Withdraw", "Exit"],
-  },
-]).then((answer) => {
-
+function operation () {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What do you want to do?",
+      choices: ["Create an Account", "Balance", "Deposit", "Withdraw", "Exit"],
+    },
+  ]).then((answer) => {
+  
     const action = answer["action"];
-
+  
     if (action === "Create an Account") {
-        createAccount();
+      createAccount();
     }
-}).catch(err => console.log(err));
+  }).catch(err => console.log(err));
+}
 
 // create an account
-
-function createAccount () {
-    console.log(chalk.bgGreen.black("Congratulations for choosing our bank!"));
-    console.log(chalk.green("Define the options of your account, as shown below."));
+function createAccount() {
+  console.log(chalk.bgGreen.black("Congratulations for choosing our bank!"));
+  console.log(chalk.green("Define the options of your account, as shown below."));
+  buildAccount();
 }
+
+function buildAccount() {
+
+  inquirer.prompt([
+    {
+      name: "accountName",
+      message: "Create an account name:"
+    }
+  ]).then(answer => {
+    
+    const accountName = answer["accountName"];
+    const accountPath = `./accounts/${accountName}.json`;
+    console.info(accountName);
+
+    if (!fs.existsSync("accounts")) {
+      fs.mkdirSync("accounts");
+    }
+
+    if (fs.existsSync(accountPath)) {
+      console.error(
+        chalk.black.bgRed(
+          `The account name ${accountName} already exists! Please choose another one.`
+        )        
+      );
+      buildAccount();
+      return
+    }
+
+    fs.writeFileSync(
+      accountPath,
+      '{"balance": 0}',
+      (err) => {
+        console.error(err);
+      }
+    );
+
+    console.info(chalk.bgGreen.black("Account created with succefully!"));
+    operation();
+
+  }).catch((err) => console.log(err));
+}
+
+operation();
