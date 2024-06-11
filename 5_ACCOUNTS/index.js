c35911ae30f22067e7440b59766408de5ee5c9df
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-06-10 21:15:12 -0300

    Lesson 78 is Done

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -5,70 +5,120 @@ const chalk = require("chalk");
 // internal modules
 const fs = require("fs");
 
-function operation () {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "action",
-      message: "What do you want to do?",
-      choices: ["Create an Account", "Balance", "Deposit", "Withdraw", "Exit"],
-    },
-  ]).then((answer) => {
-  
-    const action = answer["action"];
-  
-    if (action === "Create an Account") {
-      createAccount();
-    }
-  }).catch(err => console.log(err));
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What do you want to do?",
+        choices: [
+          "Create an Account",
+          "Deposit",
+          "Balance",
+          "Withdraw",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer["action"];
+
+      if (action === "Create an Account") {
+        createAccount();
+      } else if (action === "Deposit") {
+        deposit();
+      } else if (action === "Balance") {
+      } else if (action === "Withdraw") {
+      } else if (action === "Exit") {
+        console.clear();
+        console.log(chalk.bgBlue.black("Thank you for using Accounts App!"));
+        process.exit();
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 // create an account
+
 function createAccount() {
+  console.clear();
   console.log(chalk.bgGreen.black("Congratulations for choosing our bank!"));
-  console.log(chalk.green("Define the options of your account, as shown below."));
+  console.log(
+    chalk.green("Define the options of your account, as shown below.")
+  );
   buildAccount();
 }
 
 function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Create an account name:",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+      const accountPath = `./accounts/${accountName}.json`;
+      console.info(accountName);
 
-  inquirer.prompt([
-    {
-      name: "accountName",
-      message: "Create an account name:"
-    }
-  ]).then(answer => {
-    
-    const accountName = answer["accountName"];
-    const accountPath = `./accounts/${accountName}.json`;
-    console.info(accountName);
-
-    if (!fs.existsSync("accounts")) {
-      fs.mkdirSync("accounts");
-    }
-
-    if (fs.existsSync(accountPath)) {
-      console.error(
-        chalk.black.bgRed(
-          `The account name ${accountName} already exists! Please choose another one.`
-        )        
-      );
-      buildAccount();
-      return;
-    }
-
-    fs.writeFileSync(
-      accountPath,
-      '{"balance": 0}',
-      (err) => {
-        console.error(err);
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
       }
-    );
 
-    console.info(chalk.bgGreen.black("Account created with succefully!"));
-    operation();
+      if (fs.existsSync(accountPath)) {
+        console.error(
+          chalk.black.bgRed(
+            `The account name ${accountName} already exists! Please choose another one.`
+          )
+        );
+        buildAccount();
+        return;
+      }
 
-  }).catch((err) => console.log(err));
+      fs.writeFileSync(accountPath, '{"balance": 0}', (err) => {
+        console.error(err);
+      });
+
+      console.info(chalk.bgGreen.black("Account created with succefully!"));
+      operation();
+    })
+    .catch((err) => console.log(err));
 }
 
-operation();
+// deposit
+
+function deposit() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Enter your account name:",
+      },
+    ])
+    .then((answer) => {
+
+      const accountName = answer['accountName'];
+
+      if (!checkAccount(accountName)) {
+        return deposit();
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+function checkAccount(accountName) {
+  if (!fs.existsSync(`./accounts/${accountName}.json`)) {
+    console.log(
+      chalk.bgRed.black(
+        `The account name ${accountName} does not exist! Please try again.`
+      )
+    );
+    return false;
+  }
+
+  return true;
+}
