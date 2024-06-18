49940feb5d64cb9b6d4b90483461028e83b0e7cc
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-06-18 15:59:22 -0300

    Accounts App - Make a Transfer Section.

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -19,6 +19,7 @@ function operation() {
           "Deposit",
           "Balance",
           "Withdraw",
+          "Transfer",
           "Exit",
         ],
       },
@@ -34,6 +35,8 @@ function operation() {
         getAccountBalance();
       } else if (action === "Withdraw") {
         withdraw();
+      } else if (action === "Transfer") {
+        transfer();
       } else if (action === "Exit") {
         console.clear();
         console.log(chalk.bgBlue.black("Thank you for using Accounts App!"));
@@ -102,29 +105,28 @@ function deposit() {
       },
     ])
     .then((answer) => {
-
-      const accountName = answer['accountName'];
+      const accountName = answer["accountName"];
 
       // verify if account exists.
       if (!checkAccount(accountName)) {
         return deposit();
       }
 
-      inquirer.prompt([
-        {
-          name: "amount",
-          message: "How much do you want to deposit?"
-        }
-      ]).then((answer) => {
-        const amount = answer['amount'];
+      inquirer
+        .prompt([
+          {
+            name: "amount",
+            message: "How much do you want to deposit?",
+          },
+        ])
+        .then((answer) => {
+          const amount = answer["amount"];
 
-        //add an amount
-        addAmount(accountName, amount);
-        operation();
-
-      }).catch((err) => console.log(err))
-
-
+          //add an amount
+          addAmount(accountName, amount);
+          operation();
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
 }
@@ -136,72 +138,64 @@ function checkAccount(accountName) {
       chalk.bgRed.black(
         `The account name ${accountName} does not exist! Please try again.`
       )
-    );    
+    );
     return false;
-  }  
+  }
   return true;
 }
 
 function addAmount(accountName, amount) {
-
   const accountData = getAccount(accountName);
 
   if (!amount) {
-    errorMsg('An error occurred. Please try again later.');
+    errorMsg("An error occurred. Please try again later.");
     return deposit();
   }
 
   accountData.balance = parseFloat(amount) + parseFloat(accountData.balance);
   fs.writeFileSync(
-   `./accounts/${accountName}.json`,
-   JSON.stringify(accountData),
-   (err) => {
-    console.error(err)
-   }
+    `./accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    (err) => {
+      console.error(err);
+    }
   );
 
   console.clear();
   successMsg(`The amount of $${amount} has been deposited in your account.`);
-  
-
 }
 
 function getAccount(accountName) {
   const accountJSON = fs.readFileSync(`./accounts/${accountName}.json`, {
-    encoding: 'utf8',
-    flag: 'r'
+    encoding: "utf8",
+    flag: "r",
   });
   return JSON.parse(accountJSON);
-};
+}
 
-function errorMsg(text) {  
-  console.error(
-    chalk.bgRed.bold(text)    
-  );
-};
+function errorMsg(text) {
+  console.error(chalk.bgRed.bold(text));
+}
 
 function successMsg(text) {
-  console.log(
-    chalk.bgGreen.bold(text)
-  );
-};
+  console.log(chalk.bgGreen.bold(text));
+}
 
 function infoMsg(text) {
-  console.log(
-    chalk.bgBlue.bold(text)
-  );
-};
+  console.log(chalk.bgBlue.bold(text));
+}
 
 // show account balance
 
 function getAccountBalance() {
-  inquirer.prompt([
-    {
-      name: 'accountName',
-      message: 'Whats is your account name?'
-    }
-  ]).then(
-    (answer) => {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Whats is your account name?",
+      },
+    ])
+    .then((answer) => {
       const accountName = answer["accountName"];
 
       // verify if account exists
@@ -214,53 +208,51 @@ function getAccountBalance() {
 
       infoMsg(`Your account balance is $${accountData.balance}.`);
       operation();
-
-    }
-  ).catch((err) => console.error(err));
-};
-
+    })
+    .catch((err) => console.error(err));
+}
 
 // withdraw an amount from user account
 function withdraw() {
-  inquirer.prompt([
-    {
-      name: 'accountName',
-      message: 'Type your account name: '
-    }
-  ]).then((answer) => {
-
-    const accountName = answer['accountName'];
-    if (!checkAccount(accountName)) {
-      return withdraw();
-    }
-
-    inquirer.prompt([
+  inquirer
+    .prompt([
       {
-        name: 'amount',
-        message: 'How much to withdraw?'
+        name: "accountName",
+        message: "Type your account name: ",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+      if (!checkAccount(accountName)) {
+        return withdraw();
       }
-    ]).then((answer) => {
 
-      const amount = answer['amount']
-      removeAmount(accountName, amount);
-
+      inquirer
+        .prompt([
+          {
+            name: "amount",
+            message: "How much to withdraw?",
+          },
+        ])
+        .then((answer) => {
+          const amount = answer["amount"];
+          removeAmount(accountName, amount);
+        })
+        .catch((err) => console.log(err));
     })
-    .catch(err => console.log(err));
-
-  }).catch(err => console.log(err));
-};
+    .catch((err) => console.log(err));
+}
 
 function removeAmount(accountName, amount) {
-
   const accountData = getAccount(accountName);
 
-  if(!amount){
-    errorMsg('An error occurred. Please try again later.'); 
+  if (!amount) {
+    errorMsg("An error occurred. Please try again later.");
     return withdraw();
   }
 
-  if(accountData.balance < amount) {
-    errorMsg('Insufficient balance');
+  if (accountData.balance < amount) {
+    errorMsg("Insufficient balance");
     return withdraw();
   }
 
@@ -270,11 +262,110 @@ function removeAmount(accountName, amount) {
     `accounts/${accountName}.json`,
     JSON.stringify(accountData),
     (err) => {
-      console.log(err)
-    },
+      console.log(err);
+    }
   );
 
   successMsg(`Withdrawal of ${amount} completed successfully`);
   operation();
+}
 
-};
+function transfer() {
+  // get source account
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Please, type your account name: ",
+      },
+    ])
+    .then((answer) => {
+      const sourceAccount = answer["accountName"];
+
+      if (!checkAccount(sourceAccount)) {
+        return transfer();
+      }
+
+      // get an amount to transfer
+      inquirer
+        .prompt([
+          {
+            name: "amount",
+            message: "How much would you like to transfer? ",
+          },
+        ])
+        .then((answer) => {
+          const sourceAccountData = getAccount(sourceAccount);
+          const amount = answer["amount"];
+
+          if(!checkAmount(amount, sourceAccountData.balance)) {
+            return transfer();
+          }
+          
+
+          // get target Account
+          inquirer
+            .prompt([
+              {
+                name: "targetAccount",
+                message: "Please type the account name you want to transfer.",
+              },
+            ])
+            .then((answer) => {
+              const targetAccount = answer["targetAccount"];
+              const targetAccountData = getAccount(targetAccount);              
+
+              if (!checkAccount(sourceAccount)) {
+                return transfer();
+              }  
+              
+              if (targetAccount === sourceAccount) {
+                errorMsg("An error occurred. Please try again later.");
+                return transfer();
+              }
+
+              // make a transfer
+              sourceAccountData.balance =
+                parseFloat(sourceAccountData.balance) - parseFloat(amount);
+              targetAccountData.balance =
+                parseFloat(targetAccountData.balance) + parseFloat(amount);
+
+              fs.writeFileSync(
+                `accounts/${sourceAccount}.json`,
+                JSON.stringify(sourceAccountData),
+                (err) => {
+                  console.log(err);
+                }
+              );
+
+              fs.writeFileSync(
+                `accounts/${targetAccount}.json`,
+                JSON.stringify(targetAccountData),
+                (err) => {
+                  console.log(err);
+                }
+              );
+              successMsg(`You have transferred $${amount} from ${sourceAccount} to ${targetAccount}`);
+              operation();
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.error(err));
+    })
+    .catch((err) => console.log(err));
+}
+
+function checkAmount(amount, accountData) {
+  if (!amount) {
+    errorMsg("An error occurred. Please try again later.");
+    return false;
+  }
+
+  if (accountData < amount) {
+    errorMsg("Insufficient balance");
+    return false;
+  }
+
+  return true;
+  
+}
