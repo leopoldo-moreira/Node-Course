1d05f1bb1e0800f221e05ac5247038e407248310
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-07-10 20:02:03 -0300

    Connection Pool Done

---
+++ 8_NODE_E_MYSQL/8_connection_pool/index.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const port = 3000;
+const pool = require('./db/conn');
+
+
+
+const app = express();
+
+// tornando o BODY HTML em um objeto JSON
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// configurando o handlebars
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+('');
+// configurando os assets
+app.use(express.static('public'));
+
+//Removendo dados no db
+
+app.post('/books/remove/:id', (req, res) => {
+
+  const id = req.params.id;
+  const sql = `DELETE FROM books WHERE id = ${id}`;
+
+  pool.query(sql, (err) => {
+
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect('/books');
+
+  })
+
+
+})
+
+//Atualizando Dados no DB
+app.post('/books/updatebook', (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`;
+
+  pool.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect('/books');
+  });
+});
+
+//Editar dados do DB - 1º Passo
+app.get('/books/edit/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  pool.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const book = data[0];
+
+    res.render('editbook', { book });
+  });
+});
+
+// resgatando dados gerais do DB
+app.get('/books', (req, res) => {
+  const sql = 'SELECT * FROM books';
+
+  pool.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+    res.render('books', { books });
+  });
+});
+
+// resgatando dados especifico do DB
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  pool.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const selectedBook = data[0];
+    res.render('book', { selectedBook });
+  });
+});
+
+//inserindo dados no DB
+app.post('/books/insertbook', (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`;
+
+  pool.query(sql, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect('/');
+  });
+});
+
+// renderizando a home
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
