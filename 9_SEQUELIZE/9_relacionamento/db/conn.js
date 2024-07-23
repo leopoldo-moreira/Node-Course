d3750ac6deaf62de9e3433317d710c61b1883af7
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-07-23 20:27:00 -0300

    Relacionamento

---
+++ 9_SEQUELIZE/9_relacionamento/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+// Argumentos: nome_do_banco, usuario, senha, {host: 'hostname', dialect: 'tipo_do_banco'};
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+// try {
+//   sequelize.authenticate();
+//   console.log('Conectado ao Sequelize!');
+// } catch (err) {
+//   console.error('Não foi possivel conectar: ', err);
+// }
+
+module.exports = sequelize;
--- 9_SEQUELIZE/9_relacionamento/index.js
+++ 9_SEQUELIZE/9_relacionamento/index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 const port = 3000;
 const conn = require('./db/conn');
 const User = require('./models/User');
+const Adress = require('./models/Address');
 
 const app = express();
 
