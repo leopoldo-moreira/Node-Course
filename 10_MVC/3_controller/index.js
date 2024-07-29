90153d3c6fa484858cffd2c58c2ecc9acce316ea
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-07-28 23:55:05 -0300

    Criando Models, Controller e Rotas

---
+++ 10_MVC/3_controller/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/conn');
+const port = 3000;
+
+//models import
+const Task = require('./models/Task');
+
+const app = express();
+
+//Tornando o BODY HTML em um objeto JSON
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+//configurando o handlebars
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//configurando os assets
+app.use(express.static('public'));
+
+// iniciando o servidor
+conn
+  .sync()
+  //.sync({ force: true }) //reseta toda a tabela, perdendo os dados ja gravados.
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
