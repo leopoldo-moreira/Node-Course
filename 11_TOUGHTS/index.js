8c89d818e2b1e3a6d683ad6216910cf016423eda
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-08-30 09:39:58 -0300

    Finalizando o setup inicial

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -7,10 +7,58 @@ const port = 3000;
 const app = express();
 const conn = require('./db/conn');
 
+// template engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars')
 
-app.get('/', (req,res) => {
-    res.send('ola mundo!'); 
+// receber resposta do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+
+// session middleware
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new fileStore({
+            logFn: function () { },
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+);
+
+
+// flash messages
+app.use(flash());
+
+// public path
+app.use(express.static('public'));
+
+// set session to res
+app.use((req, res, next) => {
+
+    if(req.session.userid) {
+        res.locals.session = req.session
+    }
+
+    next();
 })
+
+
+
 
 // iniciando o servidor
 conn
