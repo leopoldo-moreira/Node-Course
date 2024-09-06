0d2a11817722c14d16c0ec4b3f4fd1e2b7bf5897
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-09-06 11:27:19 -0300

    172 - Iniciando Autenticação

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -13,6 +13,7 @@ const User = require('./models/User');
 
 // Import Routes
 const toughtsRoutes = require('./routes/ToughtsRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 // Import Controller
 const ToughtController = require('./controllers/ToughtController');
@@ -69,6 +70,7 @@ app.use((req, res, next) => {
 
 //Routes
 app.use('/toughts',toughtsRoutes)
+app.use('/', authRoutes);
 app.get('/', ToughtController.showToughts);
 
 
