2e63350eb87701faa7d7dc8471655df26893ce14
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-09-21 11:04:13 -0300

    177 e 178 done

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -8,6 +8,38 @@ module.exports = class AuthController {
         res.render('auth/login');
     }
 
+    static async loginPost(req, res) {
+
+        const { email, password } = req.body;
+
+        // find user
+        const user = await User.findOne({ where: { email: email } });
+        if (!user) {
+            req.flash('message', 'Usuário não encontrado!');
+            res.render('auth/login');
+            return;
+        }
+
+        // check if passwords match
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+        if (!passwordMatch) {
+            req.flash('message', 'Senha inválida!');
+            res.render('auth/login');
+            return;
+        }
+
+        // initialize session
+        req.session.userid = user.id;
+
+        req.flash('message', 'Login realizado com sucesso!')
+
+        req.session.save(() => {
+            res.redirect('/');
+        })
+
+
+    }
+
     static register(req, res) {
         res.render('auth/register');
     }
@@ -42,29 +74,29 @@ module.exports = class AuthController {
             password: hashedPassword,
         };
 
-    try {
-        
-        const createdUser = await User.create(user);
+        try {
 
-        // initialize session
-        req.session.userid = createdUser.id
+            const createdUser = await User.create(user);
 
-        req.flash('message', 'Cadastro realizado com sucesso!');
+            // initialize session
+            req.session.userid = createdUser.id
 
-        req.session.save(() => {
-            res.redirect('/');
-        });
+            req.flash('message', 'Cadastro realizado com sucesso!');
+
+            req.session.save(() => {
+                res.redirect('/');
+            });
 
 
-    } catch (error) {
-        console.log(error);
+        } catch (error) {
+            console.log(error);
+        }
+
     }
- 
- }
 
- static logout(req, res) {
-    req.session.destroy();
-    res.redirect('/');
- }
+    static logout(req, res) {
+        req.session.destroy();
+        res.redirect('/');
+    }
 
 }
