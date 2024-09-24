e925fa9314882b73959b9f0bf0abcfb2e00c362c
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-09-24 10:19:39 -0300

    Inserindo Pensamento no BD

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -14,4 +14,28 @@ module.exports = class ToughtController {
         res.render('toughts/create');
     }
 
+    static async createToughtSave(req, res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid,
+        }
+
+        try {
+
+            await Tought.create(tought);
+
+            req.flash('message', 'Pensamento criado com sucesso.');
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+
+    }
+
 }
