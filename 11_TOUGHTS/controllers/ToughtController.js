0d3dd68d7cd46a2d4a82b19d151b017e62254bca
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-09-27 10:00:19 -0300

    Message when dashboard is empty

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -25,7 +25,14 @@ module.exports = class ToughtController {
 
         const toughts = user.Toughts.map((result) => result.dataValues);
 
-        res.render('toughts/dashboard', { toughts });
+        let emptyToughts = false;
+
+        if (toughts.length === 0) {
+            emptyToughts = true;
+        }
+
+
+        res.render('toughts/dashboard', { toughts, emptyToughts });
     }
 
     static createTought(req, res) {
